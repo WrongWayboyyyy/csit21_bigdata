a19109821c415cbd9900c96009cd3978beb14a09
Nikita Rydanov <wrongwayboyy@gmail.com>
2021-05-15 15:43:24 +0400

    Очистка кода и комментарии к заданию 1.

--- src/main/scala/SparkDemo.scala
+++ src/main/scala/SparkDemo.scala
@@ -40,19 +40,27 @@ object SparkDemo {
 
   def sortByParam(sparkSession: SparkSession, dataFrame: DataFrame, requiredFilter: String): Unit = {
     import sparkSession.sqlContext.implicits._
+    // Парсим DataFrame как DataSet из элементов класса Case
+    val dataSet = dataFrame.as[Case]
 
-    var dataSet = dataFrame.as[Case]
-
-    var resultDataSet = dataSet
+    val resultDataSet = dataSet
+      // Группируем по требуемому признаку
       .groupBy(requiredFilter)
+      // Собираем DataFrame по группам
       .count()
+      // Сортируем по насчитанному значению
       .sort($"count".desc)
+      // Ограничиваем десятью значениями
       .limit(10)
+      // Сохраняем
       .cache()
 
     resultDataSet
+      // Пишем в BigQuery
       .write.format("bigquery")
+      // Устанавливаем переписывание существующих файлов
       .mode(SaveMode.Overwrite)
+      // Сохраняем как таблицу в файл по индивидуальному пути
       .option("table", s"data.sortBy_${requiredFilter}_output")
       .save()
   }
@@ -79,27 +87,31 @@ object SparkDemo {
       .option("escape", "\"") // Аналогично
       .option("encoding", "UTF-8") // На всякий случай
       .option("dateFormat", "yyyy-MM-dd") // Аналогично
-      .csv("gs://mmmmonkey/data.csv") //Для запуска на клауде
-    //.csv("data.csv") // Для локального запуска
+      .csv("gs://mmmmonkey/data.csv") // Для запуска на клауде
 
     import ss.sqlContext.implicits._
-
+    // Выполнение 1.1
     sortByParam(ss, df, "city_or_county")
+    // Выполнение 1.2
     sortByParam(ss, df, "state")
-
+    // Выполнение 1.3
     val dataSet = df.as[Case]
 
-    val resultDataSet: Unit = dataSet
+    dataSet
+      // Предикат, заданный в условии
       .map(x => if (x.gun_stolen.toString.contains("Stolen")
         || x.gun_stolen.toString.contains("Unknown"))
-        "Stolen || Unknown" else "Other")
+          "Stolen || Unknown" else "Other")
+      // Группируем по колонке value
+      // TODO: Понять, как самостоятельно именовать колонки или каким образом Spark именует эти колонки
       .groupBy("value")
+      // Считаем
       .count()
+      // Сохраняем аналогично функции filterByParam
       .write.format("bigquery")
       .mode(SaveMode.Overwrite)
       .option("table", s"data.gun_stolen_output")
       .save()
-
   }
 }
 
